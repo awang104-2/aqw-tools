1dd0ae4e356ebe520824d11db2b4d17e120cbdca
awang104 <63938733+awang104-2@users.noreply.github.com>
2025-01-02 13:14:21 -0800

    Updated ImageHandler.py.
    Included region parameter to optimize searching.

--- testers/tester.py
+++ testers/tester.py
@@ -1,19 +1,8 @@
-import tkinter as tk
+import numpy as np
 
-root = tk.Tk()
-root.title('Tkinter Pack Layout')
-root.geometry('600x400')
 
-frame = tk.Frame(master=root, background='light blue')
-frame.grid(columnspan=10)
-frame.rowconfigure(10, weight=1)
-
-label1 = tk.Label(master=frame, text='Tkinter', bg='red', fg='white')
-label2 = tk.Label(master=frame, text='Pack Layout', bg='green', fg='white')
-label3 = tk.Label(master=frame, text='Demo', bg='blue', fg='white')
-
-label1.grid(row=0, column=0, sticky='nsew')
-label2.grid(row=1, column=1, sticky='nsew')
-label3.grid(row=2, column=2, sticky='nsew')
-
-root.mainloop()
+test = np.array([300, 500])
+print(test)
+x, y = (100, 200)
+test += (x, y)
+print(test)
