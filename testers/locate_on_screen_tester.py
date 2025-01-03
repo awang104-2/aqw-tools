5ec695de1847d60e7604fc15716b7cabad26f1df
awang104 <63938733+awang104-2@users.noreply.github.com>
2025-01-02 22:21:54 -0800

    Reworked and updated v2.
    Updated ImageHandler.py
    Reworked Bot

--- testers/locate_on_screen_tester.py
+++ testers/locate_on_screen_tester.py
@@ -1,6 +1,11 @@
 from pyautogui import *
 
 
+im = screenshot(region=(600, 600, 600, 400))
+im.show()
+exit(1)
+
+
 path = 'tester_img.png'
 top, left, width, height = locateOnScreen(path, confidence=0.9)
 parameters = (int(top), int(left), width, height)
