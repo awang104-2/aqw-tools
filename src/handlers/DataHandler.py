88d8bdb9b8e73f374aeaaf2862508e2b3db7615c
awang104-2 <awang104@berkeley.edu>
2025-02-24 22:28:45 -0800

    foo

--- src/handlers/DataHandler.py
+++ src/handlers/DataHandler.py
@@ -4,7 +4,7 @@ import os
 
 
 def write_to_csv(data, name='logs.csv', location='../logs/'):
-    path = location + name
+    path = os.path.join(location, name)
     with open(path, 'w', newline='') as csvfile:
         fieldnames = list(data[0].keys())
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -13,13 +13,13 @@ def write_to_csv(data, name='logs.csv', location='../logs/'):
 
 
 def load_csv_as_dataframe(name, location='../logs/'):
-    path = location + name
+    path = os.path.join(location, name)
     df = pd.read_csv(path)
     return df
 
 
 def load_csv_as_records(name, location='../logs/'):
-    path = location + name
+    path = os.path.join(location, name)
     df = pd.read_csv(path)
     return df.to_dict(orient='records')
 
