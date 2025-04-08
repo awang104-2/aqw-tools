e846e36849205d0f75b7fb4ae581759172e24888
awang104-2 <awang104@berkeley.edu>
2025-04-08 01:35:11 -0700

    Updated character.py
    Adjusted __init__ for None arguments.

--- src/game/character.py
+++ src/game/character.py
@@ -1,10 +1,18 @@
 from game.combat import CombatKit
+from game.locations import Location
 
 
 class Character:
 
-    def __init__(self, *, class_name, haste):
-        self._combat_kit = CombatKit.load(class_name=class_name, haste=haste)
+    def __init__(self, *, class_name=None, haste=None, location=None):
+        if class_name and haste:
+            self._combat_kit = CombatKit.load(class_name=class_name, haste=haste)
+        else:
+            self._combat_kit = None
+        if location:
+            self._location = Location(location)
+        else:
+            self._location = None
 
     @property
     def haste(self):
@@ -20,3 +28,13 @@ class Character:
 
     def wait(self, keys):
         return self._combat_kit.wait(keys)
+
+    def initialize(self, class_name=None, haste=None, location=None):
+        if class_name and haste:
+            self._combat_kit = CombatKit.load(class_name=class_name, haste=haste)
+        else:
+            self._combat_kit = None
+        if location:
+            self._location = Location(location)
+        else:
+            self._location = None
