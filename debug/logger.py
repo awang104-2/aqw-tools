d0b0b6f3487b36f3604d72df9774133fd38df314
awang104-2 <awang104@berkeley.edu>
2025-04-14 15:25:31 -0700

    [EDITED] Debugging missing packets.

--- debug/logger.py
+++ debug/logger.py
@@ -7,15 +7,21 @@ path = os.path.dirname(__file__)
 
 class Logger:
 
-    def __init__(self, filename):
-        filepath = os.path.join(path, 'logs', filename)
-        self.logger = logging.getLogger(__name__)
+    def __init__(self, filename, name=None):
+        if not name:
+            name = __name__
+        self.filepath = os.path.join(path, 'logs', filename)
+        self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
-        file_handler = logging.FileHandler(filepath, mode='w')  # mode='w' to overwrite
+        file_handler = logging.FileHandler(self.filepath, mode='a')  # mode='w' to overwrite
         formatter = logging.Formatter('[%(asctime)s] %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
         file_handler.setFormatter(formatter)
         if not self.logger.hasHandlers():
             self.logger.addHandler(file_handler)
+
+    def clear(self):
+        with open(self.filepath, 'w'):
+            pass
 
     def debug(self, msg: str):
         self.logger.debug(msg)
