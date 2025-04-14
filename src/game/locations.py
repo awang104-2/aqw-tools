2e8ddc1b084a018d3f5ec214d53a068ba9f93897
awang104-2 <awang104@berkeley.edu>
2025-04-14 12:32:52 -0700

    Implemented recording monsters and saving to toml.

--- src/game/locations.py
+++ src/game/locations.py
@@ -1,3 +1,10 @@
+from handlers.ConfigHandler import *
+
+
+_config = get_config('locations.toml')
+LOCATIONS = SafeDict(_config)
+
+
 class Location:
 
     @staticmethod
@@ -6,35 +13,59 @@ class Location:
         return map_name.lower(), int(lobby_num)
 
     @classmethod
-    def load(cls, location):
+    def load(cls, location, with_lobby):
         if location:
-            map_name, lobby_num = Location.parse_location(location)
-            return cls(map_name, lobby_num)
+            if with_lobby:
+                map_name, lobby_num = Location.parse_location(location)
+            else:
+                map_name, lobby_num = location, None
+            monsters = LOCATIONS.get(map_name)
+            if monsters:
+                monsters = LOCATIONS.get(map_name)
+            return cls(map_name, lobby_num, monsters)
         else:
             return cls()
 
-    def __init__(self, map_name: str = None, lobby_num: int = None):
-        self._map = map_name
-        if self._map:
-            self._map.lower()
-        self._lobby = lobby_num
+    def __init__(self, map_name: str = None, lobby_num: int = None, monsters: dict = None):
+        self.map = map_name
+        if self.map:
+            self.map.lower()
+        self.lobby = lobby_num
+        if not monsters:
+            monsters = {}
+        self.monsters = monsters
 
-    @property
-    def lobby(self):
-        return self._lobby
-
-    @property
-    def map(self):
-        return self._map
-
-    def update(self, location):
-        self._map, self._lobby = Location.parse_location(location)
+    def update(self, location=None, monsters=None):
+        if location:
+            self.map, self.lobby = Location.parse_location(location)
+        if monsters:
+            self.monsters = monsters
 
     def __str__(self):
-        return f'Location: {self._map}-{self._lobby}'
+        return f'Location: {self.map}-{self.lobby}'
 
     def __repr__(self):
-        return f'{self._map}-{self._lobby}'
+        return f'{self.map}-{self.lobby}'
+
+    def add(self, monster):
+        self.monsters[monster.local_id] = monster.name
+
+    def to_dict(self):
+        return {self.map: self.monsters}
+
+    def store(self, force):
+        if not force and LOCATIONS.get(self.map):
+            return
+        dictionary = self.to_dict()
+        LOCATIONS.update(dictionary)
+
+    def save(self):
+        write_to_config(LOCATIONS, 'locations.toml')
+
+
+
+
+
 
 
 
