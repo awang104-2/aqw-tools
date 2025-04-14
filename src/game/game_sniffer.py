1e800e64baac326de0ed2cc23d379b043a6fc1ef
awang104-2 <awang104@berkeley.edu>
2025-04-14 01:20:19 -0700

    Updated for multiprocessing.

--- src/game/game_sniffer.py
+++ src/game/game_sniffer.py
@@ -1,18 +1,19 @@
 from game.aqw_backend import *
 from network.sniffing import Sniffer
-from scapy.all import Raw
+from network.layers import Raw
 
 
 class GameSniffer(Sniffer):
 
-    def __init__(self, server):
+    def __init__(self, server, *, daemon=False):
         if server not in AQW_SERVER_NAMES and server not in AQW_SERVER_IPS:
             raise ValueError('Not a valid server.')
         server = AQW_SERVERS.get(server, server)
-        super().__init__(f'tcp and src host {server}', layers=[Raw])
+        super().__init__(f'tcp and src host {server}', layers=[Raw], daemon=daemon)
+        self._server = server
 
-    def log_packet(self, packet):
-        super().log_packet(packet)
+    @property
+    def server(self):
+        return self.server
 
-    def reset(self):
-        super().reset()
+
