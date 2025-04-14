4e262e4a2f3c8a169604c5f161e9a92ea7bf64aa
awang104-2 <awang104@berkeley.edu>
2025-04-14 12:31:49 -0700

    Clean exit, implemented connecting and disconnecting.

--- src/bot/autoclicker.py
+++ src/bot/autoclicker.py
@@ -1,32 +1,47 @@
 from pywinauto import Application
+from threading import Lock
 import time
 
 
 class AutoClicker:
 
-    def __init__(self, title='GameLauncher on Artix Entertainment v.212'):
-        self.client = Application(backend='win32').connect(title=title, timeout=5)
+    def __init__(self):
+        self.client = None
+        self.ctrl = None
+        self._mouse_lock = Lock()
+        self._keyboard_lock = Lock()
+
+    def connect(self):
+        self.client = Application(backend='win32').connect(title='GameLauncher on Artix Entertainment v.212', timeout=5)
         self.ctrl = self.client.window(found_index=0)
 
+    def disconnect(self):
+        self._clear()
+        self.client = None
+        self.ctrl = None
+
     def click(self, coordinates):
-        self.ctrl.click(coords=coordinates)
+        with self._mouse_lock:
+            self.ctrl.click(coords=coordinates)
 
     def press(self, key):
-        self.ctrl.send_keystrokes(key)
+        with self._keyboard_lock:
+            self.ctrl.send_keystrokes(key)
 
     def get_hwnd(self):
         return self.ctrl.handle
 
     def type(self, string):
-        for char in string:
-            self.ctrl.send_keystrokes(char)
-            self.wait(0.1)
-        self.ctrl.send_keystrokes('{ENTER}')
+        with self._keyboard_lock:
+            for char in string:
+                self.ctrl.send_keystrokes(char)
+                self.wait(0.1)
+            self.ctrl.send_keystrokes('{ENTER}')
 
     def wait(self, t):
         time.sleep(t)
 
-    def clear(self):
+    def _clear(self):
         """
         Releases all commonly stuck keys to reset the keyboard state.
         """
@@ -46,10 +61,20 @@ class AutoClicker:
             keys_to_release.append(f"{{{char} up}}")
 
         # Release all keys
-        for key in keys_to_release:
-            self.ctrl.send_keystrokes(key)
+        with self._keyboard_lock:
+            for key in keys_to_release:
+                self.ctrl.send_keystrokes(key)
 
         print("All keys released.")
+
+    def clear(self):
+        """
+        Releases all commonly stuck keys to reset the keyboard state.
+        """
+        self._clear()
+
+    def exit(self):
+        self.disconnect()
 
 
 
