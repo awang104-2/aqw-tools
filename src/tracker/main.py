0090e6b2bbe3ca762fd5e434953599bcf2dcadc0
awang104-2 <awang104@berkeley.edu>
2025-05-02 03:40:05 -0700

    Reworking tracker app.

--- src/tracker/main.py
+++ src/tracker/main.py
@@ -1,5 +1,5 @@
 from gui import MainWindow
-from debug.logger import get_file_names, get_unique_filename
+from debug.logging import get_file_names, get_unique_filename
 from multiprocessing import active_children
 from threading import Thread, Event
 
@@ -10,9 +10,7 @@ def main():
     file_names = get_file_names()
     filename = 'log.txt'
     filename = get_unique_filename(filename, file_names)
-    character = Character()
-    updater = Updater(server='any', character=character, daemon=True, log=filename)
-    main_window = MainWindow(updater, filename)
+    main_window = MainWindow(filename)
     main_window.run()
     print('finished running')
     updater.stop()
