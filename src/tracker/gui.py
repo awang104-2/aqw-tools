3de372d01a04de37fc3e095079da31efac10b443
awang104-2 <awang104@berkeley.edu>
2025-05-08 01:20:59 -0700

    Updated and cleaned up tracker package.

--- src/tracker/gui.py
+++ src/tracker/gui.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from debug.logging import Logger
 from threading import Thread, Event, Lock
 from bot.autoclicker import AutoClicker
 from game.updater import Updater
@@ -16,21 +15,33 @@ def get_dpi_scaling(root):
 
 class MainWindow:
 
+    width = 400
+    height = 300
+    x_offset = 0
+    y_offset = 0
+
     def __init__(self):
-        self.updater = Updater()
+        # Tkinter Window
         self.root = Tk()
+        self.scaling_factor = get_dpi_scaling(self.root)
+        self.width = int(self.width * self.scaling_factor)
+        self.height = int(self.height * self.scaling_factor)
+        self.x_offset = int(self.x_offset * self.scaling_factor)
+        self.y_offset = int(self.y_offset * self.scaling_factor)
+        self.root.geometry(f'{self.width}x{self.height}-{self.x_offset}+{self.y_offset}')
         self.root.protocol("WM_DELETE_WINDOW", self.quit)
+
+        # Labels
+        self.name = Label(self.root, font=("Arial", 16, "bold"))
+        self.name.pack()
+        self.info = Label(self.root, font=("Arial", 12, "normal"))
+        self.info.pack()
+        self.status = Label(self.root, font=("Arial", 12, "normal"))
+        self.status.pack()
+
+        # Buttons
         self.frame = Frame(self.root)
         self.frame.pack(side='bottom', pady=20)
-        self.scaling_factor = get_dpi_scaling(self.root)
-        self.root.geometry(f'{int(250 * self.scaling_factor)}x{int(200 * self.scaling_factor)}-0+0')
-        self.root.attributes('-topmost', True)
-        self.name = Label(self.root, font=("Arial", 12, "bold"))
-        self.name.pack()
-        self.info = Label(self.root, font=("Arial", 8, "normal"))
-        self.info.pack()
-        self.status = Label(self.root, font=("Arial", 8, "normal"))
-        self.status.pack()
         self.connect_button = Button(self.frame, text='START', command=self.start_updating)
         self.connect_button.pack(side='left')
         self.reset_button = Button(self.frame, text='RESET', command=self.reset)
@@ -39,10 +50,11 @@ class MainWindow:
         self.save_button.pack(side='left')
         self.fight_button = Button(self.frame, text='FIGHT', command=self.fight)
         self.fight_button.pack(side='left')
-        self._lock = Lock()
-        self.logger = Logger(log, 'main window')
-        self.flag = Event()
+
+        # Other
+        self.updater = Updater(character=Character(), server='twig')
         self.autoclicker = AutoClicker()
+        self.flag = Event()
         self._last_after = None
 
     def fight(self):
@@ -54,54 +66,36 @@ class MainWindow:
             self.fight_button.config(text='RUN TEST')
 
     def combat_loop(self):
-        character = self.updater.character
         self.flag.set()
-        self.autoclicker.connect()
         while True:
-            for i in ['1', '2', '3', '4', '5']:
-                self.autoclicker.press(i)
+            for key in ['1', '2', '3', '4', '5']:
+                self.autoclicker.press(key)
                 self.autoclicker.wait(0.1)
-                if character.crit_data().get('total') >= 2000:
-                    self.fight()
                 if not self.flag.is_set():
-                    self.autoclicker.disconnect()
                     return
 
     def _reset(self):
-        print(f'called reset')
-        print(f'acquiring lock')
-        with self._lock:
-            if not self.updater.connected('any'):
-                self.updater = Updater()
-            self.connect_button.config(state='normal')
-        print(f'lock released')
+        self.updater.reset()
+        self.connect_button.config(state='normal')
+
+    def _start_updating(self):
+        self.updater.start()
+
+    def _stop_updating(self):
+        self.updater.stop()
 
     def reset(self):
         Thread(target=self._reset).start()
 
-    def _start_updating(self):
-        with self._lock:
-            self.updater.connect()
-            self.updater.start()
-
-    def _stop_updating(self):
-        with self._lock:
-            self.updater.stop()
-            self.updater.disconnect()
-
     def start_updating(self):
         Thread(target=self._start_updating).start()
-        self.logger.info('Changing Start/Stop button to Start.')
         self.connect_button.config(text='STOP', command=self.stop_updating)
-        self.logger.info('Disabling reset and save buttons.')
         self.reset_button.config(state='disabled')
         self.save_button.config(state='disabled')
 
     def stop_updating(self):
         Thread(target=self._stop_updating).start()
-        self.logger.info('Changing Start/Stop button to Stop.')
         self.connect_button.config(state='disabled', text='START', command=self.start_updating)
-        self.logger.info('Enabling reset and save buttons.')
         self.reset_button.config(state='normal')
         self.save_button.config(state='normal')
 
@@ -117,7 +111,6 @@ class MainWindow:
         self._last_after = self.root.after(time, self.after, time)
 
     def run(self):
-        self.logger.info('Running main window.')
         self.after()
         self.mainloop()
 
--- src/tracker/main.py
+++ src/tracker/main.py
@@ -1,15 +1,9 @@
 from gui import MainWindow
-from debug.logging import get_file_names, get_unique_filename
-from multiprocessing import active_children
-from threading import Thread, Event
 
 
 def main():
-    children = active_children()
-    for child in children:
-        print(f'Terminating {child}.')
-        child.terminate()
-        child.join()
+    main_window = MainWindow()
+    main_window.run()
 
 
 if __name__ == '__main__':
